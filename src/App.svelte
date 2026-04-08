f0b667387f69920d3f67c50ac0806b6bfb981b52
Rasacalpaulk <racorredormedina@gmail.com>
2026-04-08 10:33:27 -0600

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,9 @@
   import Counter from './components/Counter.svelte'
   import Header from './components/Header.svelte';
   import Sb from './components/Sb.svelte';
-</script>
+  import Footer from './components/Footer.svelte';
+  import NuestraVision from './components/NuestraVision.svelte';
+</script>   
 
 <Header></Header>
 <Sb></Sb>
@@ -26,11 +28,7 @@
 </section>
 
 <section id="vision">
-  <div class="la_vision">
-    <h1>Nuestra Visión</h1>
-    <p>Ser la empresa líder en el mercado de remodelaciones y tecnología estructural 
-      al igual que en el apartado de seguridad cibernética. </p>
-  </div>
+  <NuestraVision />
 </section>
 <div class="ticks"></div>
 <section id="next-steps">
@@ -134,7 +132,7 @@
 <section id="spacer"></section>
 
 <Footer>
-
+  
 </Footer>
 
 <style>
