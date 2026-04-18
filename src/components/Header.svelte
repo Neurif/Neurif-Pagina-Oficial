b81a3ed03327f2c3be537b633ce448c5430cc0a9
Pablo0908 <pablofori09@gmail.com>
2026-04-18 15:50:06 -0600

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,14 +3,16 @@
 </script>
 
 <header>
-    <div class="logo-header">
-        <img src={logo} alt="logo">
-    </div>
-    <div class="barra-navegacion-header">
+<div class="barra-navegacion-header">
         <a href="mision">Misión</a>
         <a href="vision">Visión</a>
         <a href="sobre-nosotros">Sobre Nosotros</a>
+</div>
+    <div class="logo-header">
+        <img src={logo} alt="logo" style="width: 100px; height: 100px;"/>
     </div>
+        <a href="contacto">Contacto</a>
+        <a href="blog">Blog</a>
 </header>
 
 <style>
