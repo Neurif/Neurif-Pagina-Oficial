911f2847130bb316e834739f7057391b0185ef3f
srdelta456-netizen <srdelta456@gmail.com>
2026-04-17 21:11:17 -0600

    Footer v2
    Chetos

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,26 @@
 <script>
+import instagramIcon from '/instagram.svg';
 
+import tiktokIcon from '/tiktok.svg';
+import youtubeIcon from '/youtube.svg';
 </script>
 
 <footer>
-    
+    <div class="footer-card">
+            <nav class="footer-enlaces">
+                <strong>Enlaces rápidos</strong>
+                <div>Inicio</div>
+                <div>Productos</div>
+                <div>Sobre Nosotros</div>
+            </nav>
+            <div class="footer-redes">
+                <strong>Redes Sociales</strong>
+                <div class="footer-icons">
+                    <img src={instagramIcon} alt="Instagram">
+                    <img src={tiktokIcon} alt="TikTok">
+                    <img src={youtubeIcon} alt="YouTube">
+                </div>
+            </div>
+        </div>
+        <strong class="footer-copyright">© Neurif</strong>
 </footer>
